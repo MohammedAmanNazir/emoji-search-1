42510b354e1fc717ab0156fccce1b45d6fd2fc3f
MOHAMMED AMAN NAZIR <106735478+MohammedAmanNazir@users.noreply.github.com>
2022-10-09 18:46:02 +0530

    Add files via upload

---
+++ build/precache-manifest.0c0201fd6c78ad02cd028316e76f8e0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8147c730ed420dc66a702684eef769c",
+    "url": "/emoji-search/index.html"
+  },
+  {
+    "revision": "3896f89b5a26c3da5987",
+    "url": "/emoji-search/static/css/2.5510c45e.chunk.css"
+  },
+  {
+    "revision": "bd53b86d6597364b6ab7",
+    "url": "/emoji-search/static/css/main.f38963d9.chunk.css"
+  },
+  {
+    "revision": "3896f89b5a26c3da5987",
+    "url": "/emoji-search/static/js/2.4eecd746.chunk.js"
+  },
+  {
+    "revision": "6120b983a03909b7d2cd9bf134169811",
+    "url": "/emoji-search/static/js/2.4eecd746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd53b86d6597364b6ab7",
+    "url": "/emoji-search/static/js/main.666e619b.chunk.js"
+  },
+  {
+    "revision": "53f65d824d8bd704bdf5",
+    "url": "/emoji-search/static/js/runtime-main.fcefcbdb.js"
+  }
+]);
